e36c489d659c6af2f299bfa3e0e985779bc02267
TuanTrong1108 <TuanTrong1108@gmail.com>
2022-12-06 01:36:07 +0700

    test

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+
+// const menu = document.querySelector(".navbar__links");
+// const menuButton = document.querySelector(".navbar__icons");
+
+
+
+// menuButton.addEventListener("click", () => {
+//     menu.classList.toggle("navbar__open");
+
+// });
+
+
+
+const menu = document.querySelector(".navbar__links");
+const menuButton = document.querySelector(".navbar__icons");
+
+menuButton.addEventListener("click", () => {
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+})
